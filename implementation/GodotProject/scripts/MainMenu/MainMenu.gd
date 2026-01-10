5bda7d78b07ac5257f9e156e3b2c0d659e54dca7
Daniel Batinić <daniel.batinic@fer.hr>
2026-01-10 19:01:37 +0100

    Update main menu and add new scene

--- implementation/GodotProject/scripts/MainMenu/MainMenu.gd
+++ implementation/GodotProject/scripts/MainMenu/MainMenu.gd
@@ -1,22 +1,23 @@
 extends Control
 
-@onready var transitions = $MainMenuTransitions
-@onready var scenario_selector = $ScenarioSelector
-@onready var button_start = $VBox/ButtonStart
-@onready var button_options = $VBox/ButtonOptions
-@onready var button_quit = $VBox/ButtonQuit
 
-func _ready():
-	button_start.pressed.connect(_on_start_pressed)
-	button_options.pressed.connect(_on_options_pressed)
-	button_quit.pressed.connect(_on_quit_pressed)
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
-func _on_start_pressed():
-	transitions.show_scenario_selector()
 
-func _on_options_pressed():
-	# Placeholder for options menu
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
 	pass
 
-func _on_quit_pressed():
+
+
+func _on_exit_btn_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_option_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/MainMenu/Options.tscn")
+
+func _on_start_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/MainMenu/ScenarioSelector.tscn")
--- implementation/GodotProject/scripts/MainMenu/ScenarioSelector.gd
+++ implementation/GodotProject/scripts/MainMenu/ScenarioSelector.gd
@@ -1,58 +1,15 @@
 extends Control
 
-# Scenario selector for main menu
 
-signal scenario_selected(scenario_data)
-
-var selected_scenario = null
-
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
 
-func _ready():
-	var scenario_dir = "res://assets/Scenarios"
-	var scenario_paths = []
-	var dir = DirAccess.open(scenario_dir)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if file_name.ends_with(".tres"):
-				scenario_paths.append(scenario_dir + "/" + file_name)
-			file_name = dir.get_next()
-		var scenarios = []
-		for path in scenario_paths:
-			var res = load(path)
-			if res:
-				scenarios.append(res)
-		set_scenarios(scenarios)
-
-	$Footer/BackButton.pressed.connect(_on_back_pressed)
-	$Footer/StartButton.pressed.connect(_on_start_pressed)
-
-func _on_back_pressed():
-	get_parent().get_node("MainMenuTransitions").show_main_menu()
-
-func _on_start_pressed():
-	if selected_scenario and selected_scenario.level_scene:
-		get_tree().change_scene_to_packed(selected_scenario.level_scene)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
 
 
-func set_scenarios(scenarios: Array):
-	var grid = $Scroll/Grid
-	for c in grid.get_children():
-		c.queue_free()
-	for scenario in scenarios:
-		var item = preload("res://scenes/UI/MainMenu/ScenarioGridItem.tscn").instantiate()
-		item.setup(scenario)
-		item.connect("selected", Callable(self, "_on_scenario_selected"))
-		grid.add_child(item)
-
-func _on_scenario_selected(item):
-	for c in $Scroll/Grid.get_children():
-		c.modulate = Color(1,1,1,1)
-	item.modulate = Color(0.5,0.8,1,1)
-	select_scenario(item.scenario_data)
-
-func select_scenario(scenario_data):
-	selected_scenario = scenario_data
-	emit_signal("scenario_selected", scenario_data)
+func _on_return_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/MainMenu/MainMenu.tscn")
